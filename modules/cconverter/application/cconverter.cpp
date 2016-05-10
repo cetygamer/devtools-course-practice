804dba50c79dbab7e64eceb5d638ef80c0bf1bec
cetygamer <cetygamer@gmail.com>
2016-05-10 22:31:48 +0300

    Remove unnecessary builds

--- modules/cconverter/application/cconverter.cpp
+++ modules/cconverter/application/cconverter.cpp
@@ -4,14 +4,24 @@
 #include <stdio.h>
 #include <string>
 
+#include <fstream>
+
 #include "include/currency_converter.h"
 
-int main(int argc, const char** argv) {
-  //CurrencyConverter app;
-  //std::string output = app(argc, argv);
-  //printf("%s\n", output.c_str());
-  
+int main(int argc, const char* argv[]) {
+  // CurrencyConverter app;
+  // std::string output = app(argc, argv);
+  // printf("%s\n", output.c_str());
+
   printf("Hello World!\n");
+
+  std::ofstream fout("cpairs_db.txt");
+
+  fout << "RUB\\USD 1.2 3.4";
+
+  fout.close();
+
+  printf(argv[0]);
 
   return 0;
 }
